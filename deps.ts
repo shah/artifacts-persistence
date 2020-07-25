aa504a8c7e391afdf180593bc269ea102d778de8
Shahid N. Shah <shahid@shah.org>
2020-07-25 11:03:30 -0400

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.4/mod.ts";
-export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.3/mod.ts";
-export * as valueMgr from "https://denopkg.com/shah/value-manager@v1.0.3/mod.ts";
+export * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.5/mod.ts";
+export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.4/mod.ts";
+export * as valueMgr from "https://denopkg.com/shah/value-manager@v1.0.5/mod.ts";
 export * as stdEncodeYAML from "https://deno.land/std@v0.62.0/encoding/yaml.ts";
 export * as stdFS from "https://deno.land/std@v0.62.0/fs/mod.ts";
 export * as stdIO from "https://deno.land/std@v0.62.0/io/mod.ts";

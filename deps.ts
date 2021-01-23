da57cf190179c658fcc204d6694dceaffd8108a3
Shahid N. Shah <shahid@shah.org>
2021-01-23 12:49:55 -0500

    fix: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,9 +1,10 @@
-export * as stdEncodeYAML from "https://deno.land/std@0.80.0/encoding/yaml.ts";
-export * as stdFS from "https://deno.land/std@0.80.0/fs/mod.ts";
-export * as stdIO from "https://deno.land/std@0.80.0/io/mod.ts";
-export * as stdPath from "https://deno.land/std@0.80.0/path/mod.ts";
-export * as stdAsserts from "https://deno.land/std@0.80.0/testing/asserts.ts";
-export * as denoLibPrettier from "https://denolib.com/denolib/prettier/prettier.ts";
+export * as stdEncodeYAML from "https://deno.land/std@0.84.0/encoding/yaml.ts";
+export * as stdEncodeTOML from "https://deno.land/std@0.84.0/encoding/toml.ts";
+export * as stdFS from "https://deno.land/std@0.84.0/fs/mod.ts";
+export * as stdIO from "https://deno.land/std@0.84.0/io/mod.ts";
+export * as stdPath from "https://deno.land/std@0.84.0/path/mod.ts";
+export * as stdAsserts from "https://deno.land/std@0.84.0/testing/asserts.ts";
+export * as denoLibPrettier from "https://denopkg.com/denolib/prettier/prettier.ts";
 export * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.7/mod.ts";
 export * as serializeJS from "https://denopkg.com/shah/javascript-stringify@v2.0.4/src/index.ts";
 export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.5/mod.ts";
